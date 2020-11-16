d766c4a1ec2f609ae95368de79a97deb748adc7f
Ehsan Nayernia <62602163+Ehsannyr@users.noreply.github.com>
2020-11-16 21:29:44 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-#authors: amirhosein ataei-ehsan nayernia
+#authors: ehsan nayernia-amirhosein ataei
 
 import numpy as np
 
@@ -10,17 +10,19 @@ a = text.readline()
 i = [0]*3
 for c in range(len(a)):
     if a[c] == '0':
-        i[0]=i[0]+1
+        i[0]=i[0]+1  
     if a[c] == '1':
-        i[1]=i[1]+1
+        i[1]=i[1]+1  
     if a[c] == '2':
         i[2]=i[2]+1
-
+      
 ## plot section
 fig = plt.figure()
 nums = ['0', '1', '2']
 barlist = plt.bar(nums, i, width=1)
 barlist[0].set_color('g')
-barlist[1].set_color('w')
+barlist[1].set_color('b')
 barlist[2].set_color('r')
+
+plt.savefig('out.png', format='png', dpi=160)
 plt.show()
